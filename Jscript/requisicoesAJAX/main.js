8ce5a8502276039e1fcf955a8539b1cc4a254509
joao vitor <jvguedes4@gmail.com>
2020-04-01 10:51:57 -0300

    Jscript UP

---
+++ Jscript/requisicoesAJAX/main.js
@@ -0,0 +1,48 @@
+//Definição de Ajax Codigo Assincrono.
+// var xhr = new XMLHttpRequest(); //Variavel para definição AJAX
+// xhr.open('GET','https://api.github.com/users/diego3g')
+// xhr.send(null);
+
+// xhr.onreadystatechange = function(){
+//     if(xhr.readyState ===4){
+//         console.log(JSON.parse(xhr.responseText))
+//     }
+// }
+
+/*Tratamento do Codigo Assincrono com Promise
+
+ var minhaPromise = function(){
+     return new Promise(function(resolve, reject){
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET','https://api.github.com/users/diego3g');
+        xhr.send(null);
+
+        xhr.onreadystatechange = function(){
+            if(xhr.readyState === 4){
+                if(xhr.status === 200){
+                    resolve(JSON.parse(xhr.responseText));
+                }else{
+                    reject('Erro na requisição');
+                }
+            }
+        }
+
+     });
+ }
+ 
+ minhaPromise()
+    .then(function(response){
+        console.log(response);
+    })
+    .catch(function(error){
+        console.warn(error);
+    })*/
+
+   //Requisicoes com Axios. Melhor Metodo!!!
+    axios.get('https://api.github.com/users/diego3g')
+    .then(function(response){
+        console.log(response);
+    })
+    .catch(function(error){
+        console.warn(error);
+    })
